4c81c8f2b4626214d46118a8ea92cd3297a82a62
Ram Sharma <sharmashekhar20050@gmail.com>
2025-10-29 11:21:20 +0545

    Server url modified

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
   const [tasks, setTasks] = useState([]);
   const [copyTask, setCopy] = useState([]);
   const fetchData = async () => {
-    const { data } = await axios.get("http://locahost:5000/todo");
+    const { data } = await axios.get("http://localhost:5000/todo");
     setTasks(data.data);
     setCopy(data.data);
   };
@@ -19,7 +19,7 @@ const Home = () => {
       isDone: false,
     };
     try {
-      const { data } = await axios.post("http://locahost:5000/todo", obj);
+      const { data } = await axios.post("http://localhost:5000/todo", obj);
       if (data.success) {
         toast.success(data.message);
         setText("");
@@ -39,7 +39,7 @@ const Home = () => {
       };
       try {
         const { data } = await axios.put(
-          `http://locahost:5000/todo/${isEdit._id}`,
+          `http://localhost:5000/todo/${isEdit._id}`,
           obj
         );
         if (data.success) {
@@ -57,7 +57,7 @@ const Home = () => {
   };
   const handleDelete = async (id) => {
     try {
-      const { data } = await axios.delete("http://locahost:5000/todo/" + id);
+      const { data } = await axios.delete("http://localhost:5000/todo/" + id);
       if (data.success) {
         toast.success(data.message);
         setText("");
@@ -76,7 +76,7 @@ const Home = () => {
       isDone: !isDone,
     };
     try {
-      const { data } = await axios.put(`http://locahost:5000/todo/${id}`, obj);
+      const { data } = await axios.put(`http://localhost:5000/todo/${id}`, obj);
       if (data.success) {
         toast.success(data.message);
         fetchData();
